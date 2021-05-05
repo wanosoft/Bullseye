35fc3e0ee1c421d18d3ebd6cd038bc13ac45b5e4
David Arellano <david.ahrellano@gmail.com>
2021-05-05 12:24:49 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by David Arellano on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
